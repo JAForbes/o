99dc9f5761641f57f136ae241ccd1df84cb76a38
James <james.a.forbes@gmail.com>
2014-07-30 13:19:17 +1000

    Now, even internally you editing hash is impossible for any function other than hash() and remove()

--- o.js
+++ o.js
@@ -8,30 +8,72 @@ function o(hash,changeCallback){
 		if(key && value){
 			return set(key,value)
 		} else if(key) {
-			return hash[key]
+			return hash(key)
 		}
-		return copy(hash);
+		return hash();
 	}
 
-	//return a copy of the internal hash
+	/*
+		close over hash, and replace with hash()
+		Only remove() and hash() can mutate hash.
+		Other functions have to go through hash()
+	*/
+
+	(function(original){
+
+		entry.remove = function(keys){
+			if(arguments.length > 1){
+			  keys = ([]).slice.call(arguments);
+			}
+			if(Array.isArray(keys)){
+				for(var i =0; i< keys.length; i++){
+					entry.remove(keys[i])
+				}
+			} else {
+				var key = keys;
+				delete original[key]
+				delete entry[key]
+				changeCallback && changeCallback(null,key,hash())
+				callbacks[key] && callbacks[key](val,key,hash())
+				delete callbacks[key]
+			}
+			return entry;
+		}
+
+
+		hash = function(key,val){
+			if(key && val){
+				return original[key] = val
+			} else if (key) {
+				return original[key]
+			} else {
+				return copy(original)
+			}
+		}
+
+	})(hash || {})
+
+
+
+	//return a copy of a hash
 	function copy(obj){
-		var _copy = {};
+		var copied = {};
 		each(obj,function(val,key){
-	      _copy[key] = val;
+			copied[key] = val;
 		});
-		return _copy;
+		return copied;
 	}
 
 	//set a val in the hash, trigger change and create a getter/setter
 	function set(key,val){
   		//set value
-  		hash[key] = val;
+  		hash(key,val)
   		//call change callback
-  		changeCallback && changeCallback(val,key,hash)
+  		changeCallback && changeCallback(val,key,hash())
   		//create setter
-  		entry[key] = attr(key)
+  		entry[key] = createAccessor(key)
   		entry[key].change = acceptAttrChange(key)
-  		callbacks[key] && callbacks[key](val,key,hash)
+  		callbacks[key] && callbacks[key](val,key,hash())
   		return entry;
 	}
 
@@ -42,49 +84,33 @@ function o(hash,changeCallback){
 	}
 
 	//iterate through an object
-	function each(hash,iterator){
-		for(var k in hash){
-			var v = hash[k];
+	function each(obj,iterator){
+		for(var k in obj){
+			var v = obj[k];
 			iterator(v,k)
 		}
-	}
-
-	//creates an attribute getter/setter
-	function attr(key){
-	  return function(val){
-	  	if(val){
-	  		return set(key,val)
-	  	}
-      	return hash[key]
-      }
 	}
 
 	function isArray(array){
 		return !!array.concat
 	}
 
-	entry.remove = function(keys){
-		if(Array.isArray(keys)){
-			for(var i =0; i< keys.length; i++){
-				entry.remove(keys[i])
-			}
-		} else {
-			var key = keys;
-			delete hash[key]
-			delete entry[key]
-			changeCallback(null,key,hash)
-			callbacks[key] && callbacks[key](val,key,hash)
-			delete callbacks[key]
-		}
-		return entry;
+	//creates an attribute getter/setter
+	function createAccessor(key){
+	  return function(val){
+	  	if(val){
+	  		return set(key,val)
+	  	}
+      	return hash(key)
+      }
 	}
 
 	entry.change = function(onchange){
 		changeCallback = onchange
 		return entry;
 	}
-
-	each(hash,function(val,key){
+	
+	each(hash(),function(val,key){
       set(key,val)
 	});
 
